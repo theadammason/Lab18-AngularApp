ebcdd86e93bd3dd6611043cb34d94c15e92320e3
theadammason <adam.mason86@gmail.com>
2017-03-01 21:39:34 -0500

    added ng-repeat

--- js/controllers/fbControl.js
+++ js/controllers/fbControl.js
@@ -2,12 +2,15 @@ var app = angular.module('myMod');
 
 app.controller('fbControl', function($scope, fbFactory){
 
+$scope.results = [];
+
 $scope.runFizzBuzz = function(input){
-  $scope.result = fbFactory.fizzbuzz(input);
+  $scope.results.push(fbFactory.fizzbuzz(input));
+  console.log($scope.results);
   if (!$scope.display){
     $scope.display = !$scope.display;
   }else{
-
   };
+
 }
 });
